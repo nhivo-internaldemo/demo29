df1f86d5d71fff2da67740b076cd67f5a00ed603
nhivo <nhivo@192.168.1.90>
2021-09-29 11:29:59 +0700

    add to my file

---
+++ Test Suites/Demo test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0ef3cd92-55ad-4c0d-a7b7-86bfb95d65b3</testSuiteGuid>
+</TestSuiteEntity>
